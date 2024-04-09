adfb7ddc8bfd3042513225e8a515d7df4f7b4495
Mark Wharton <mark.wharton@iotics.com>
2024-04-09 12:52:22 +0100

    refactored code to use an object for API access and the main() code to use it "Fixed" move_to_received

--- testapi.py
+++ testapi.py
@@ -1,8 +1,10 @@
+'''FocalBoard API integration code'''
+# import json
+import os
+from pprint import pprint
 import requests
 from dotenv import load_dotenv
-import json
-import os
-import jmespath
+# import jmespath
 
 # API gubbins here: https://htmlpreview.github.io/?https://github.com/mattermost/focalboard/blob/main/server/swagger/docs/html/index.html
 
@@ -11,150 +13,138 @@ import jmespath
 # user_pass = Focalboard password
 # repair_cafe_date = next date (needs to be in the title of the board)
 
-load_dotenv()
+class FbApiException(Exception):
+    '''Exception caused by bad API-ness'''
 
-url = "https://focalboard.zx85.me/api/v2"
-# This will likely change from board to board chiz chiz
-received_tuple = {"a9ctq6eyxy9pkcc18dpot439bsw": "ao9w4t3desyyektq1qrcz6nsxzw"}
+class FbApiWrapper:
+    '''Wrapper class to contain all the API functions and variables'''
+    __URL = "https://focalboard.zx85.me/api/v2"
+    __headers = {
+        "Accept": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "application/json",
+    }
+
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+
+    def __enter__(self):
+        token = self.__do_login()
+        if token:
+            self.__headers["Authorization"] = f"Bearer {token}"
+        else:
+            raise FbApiException
+        return self
+ 
+    def __exit__(self, *args):
+        pass
+
+    def __do_login(self):
+        '''(private) Login'''
+        api_token = None
+        with requests.Session() as s:
+            credentials = {"type": "normal", "username": self.__username, "password": self.__password}
+            response = s.post(self.__URL + "/login", headers=self.__headers, json=credentials)
+            resp = response.json()
+            api_token = resp["token"]
+        return api_token
 
 
-def do_login(url, username, password):
-    token = ""
-    try:
-        credentials = {"type": "normal", "username": username, "password": password}
-        headers = {
-            "Accept": "application/json",
-            "X-Requested-With": "XMLHttpRequest",
-            "Content-Type": "application/json",
+    def get_teams(self):
+        '''get the list of teams'''
+        teams = []
+        with requests.Session() as s:
+            response = s.get(self.__URL + "/teams", headers=self.__headers)
+            teams = response.json()
+        return teams
+
+
+    def get_blocks(self, board_id):
+        '''get the list of blocks'''
+        blocks = []
+        with requests.Session() as s:
+            response = s.get(self.__URL + "/boards/" + board_id + "/blocks", headers=self.__headers)
+            blocks = response.json()
+        return blocks
+
+
+    def get_boards(self, team):
+        '''get the list of boards'''
+        boards = []       
+        with requests.Session() as s:
+            response = s.get(self.__URL + "/teams/" + team + "/boards", headers=self.__headers)
+            boards = response.json()
+        return boards
+
+
+    def get_cards(self, board_id):
+        '''get the list of cards'''
+        cards = []
+        with requests.Session() as s:
+            response = s.get(self.__URL + "/boards/" + board_id + "/cards", headers=self.__headers)
+            cards = response.json()
+        return cards
+
+
+    def create_card(self, board_id, title):
+        '''create a card'''
+        response = {}
+        card_details = {
+            "title": title,
         }
-        session = requests.Session()
-        req = session.post(url + "/login", json=credentials, headers=headers)
-        if req.status_code == 200:
-            token = req.json()["token"]
-
-    except Exception as e:
-        print(f"Failed to login because: {str(e)}")
-    return token
+        with requests.Session() as s:
+            response = s.post(self.__URL + "/boards/" + board_id + "/cards", json=card_details, headers=self.__headers)
+            response = response.json()
+        return response["id"]
 
 
-def get_teams(url, token):
-    teams = []
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-    req = requests.get(url + "/teams", headers=headers)
-    if req.status_code == 200:
-        teams = req.json()
-    return teams
+    def move_card_to_received(self, board_id, card_id):
+        '''move a card to received - EXPERIMENTAL'''
+        response = {}
+        # TODO how the hell do we know what the magic status field is called and what to set it to?
+        patch_json = {
+            "updatedFields": {
+                "properties": {
+                    "a9ctq6eyxy9pkcc18dpot439bsw": "ao9w4t3desyyektq1qrcz6nsxzw"
+                },
+            },
+            "deletedFields": []
+        }
 
+        # https://focalboard.zx85.me/api/v2/boards/bidz77spmbp875ept8566kykcar/blocks/c5yipweto7bbrfftpa99dpx9pra
 
-def get_blocks(url, token, board_id):
-    blocks = []
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-    req = requests.get(url + "/boards/" + board_id + "/blocks", headers=headers)
-    if req.status_code == 200:
-        blocks = req.json()
-    else:
-        print(f"Oh no... status code {req.status_code}")
-    return blocks
-
-
-def get_boards(url, token, team):
-    boards = []
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-    req = requests.get(url + "/teams/" + team + "/boards", headers=headers)
-    if req.status_code == 200:
-        boards = req.json()
-    return boards
-
-
-def get_cards(url, token, board_id):
-    cards = []
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-    req = requests.get(url + "/boards/" + board_id + "/cards", headers=headers)
-    if req.status_code == 200:
-        cards = req.json()
-    return cards
-
-
-def create_card(url, token, board_id, title):
-    response = {}
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-    card_details = {
-        "title": title,
-    }
-    req = requests.post(
-        url + "/boards/" + board_id + "/cards", json=card_details, headers=headers
-    )
-    if req.status_code == 200:
-        response = req.json()
-    return response["id"]
-
-
-def move_card_to_received(url, token, card_id, received_tuple):
-    response = {}
-    headers = {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-        "Authorization": "Bearer " + token,
-    }
-
-    req = requests.patch(
-        url + "/cards/" + card_id,
-        json=received_tuple,
-        headers=headers,
-    )
-    if req.status_code == 200:
-        # response = req.json()
-        response = req.text
-        print(response)
-    return response
+        with requests.Session() as s:
+            response = s.patch(self.__URL + "/boards/" + board_id + "/blocks/" + card_id, json=patch_json, headers=self.__headers)
+            # response = req.text
+            # pprint(response.json())
+        return response
 
 
 def main():
+    '''main'''
+    load_dotenv()
     username = os.getenv("user_name")
     password = os.getenv("user_pass")
     repair_cafe_date = os.getenv("repair_cafe_date")
-    token = do_login(url, username, password)
-    first_team_id = get_teams(url, token)[0]["id"]  # just one team right?
-    board_id = [
-        each_board["id"]
-        for each_board in get_boards(url, token, first_team_id)
-        if repair_cafe_date in each_board["title"]
-    ][0]
-    cards = get_cards(url, token, board_id)
-    # print(json.dumps(cards))
 
-    # This bit creates a new card
-    title = "Trying to patch"
-    new_card_id = create_card(url, token, board_id, title)
-    print(f"New card ID is {new_card_id}")
+    with FbApiWrapper(username, password) as api:
 
-    # Haven't worked out how to change the status (eg to RECEIVED)
-    # eg it does a PATCH to cb3ehj1e4ijg3pfqf383pri6wnr
-    # With properties like "a9ctq6eyxy9pkcc18dpot439bsw": "ao9w4t3desyyektq1qrcz6nsxzw"
-    # (As seen in developer mode on the browser)
-    # TODO: get block_id from blocks and then patch that block?
-    move_card_to_received(url, token, new_card_id, received_tuple)
-    # looks like this does something but randomly it creates a new card?
+        first_team_id = api.get_teams()[0]["id"]  # just one team right?
+        board_id = [
+            each_board["id"]
+            for each_board in api.get_boards(first_team_id)
+            if repair_cafe_date in each_board["title"]
+        ][0]
+        cards = api.get_cards(board_id)
+        # print(json.dumps(cards))
+
+        # This bit creates a new card
+        title = "Trying to patch"
+        new_card_id = api.create_card(board_id, title)
+        print(f"New card ID is {new_card_id}")
+
+        api.move_card_to_received(board_id, new_card_id)
 
 
 if __name__ == "__main__":
